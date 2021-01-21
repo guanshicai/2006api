275f03f6887c2983327fdf8bb8353157d0549f17
unknown <384187225@qq.com>
2021-01-21 09:06:01 +0800

    第一次提交

--- user.php
+++ user.php
@@ -18,7 +18,25 @@
     $row = $res->fetch(PDO::FETCH_ASSOC);
 //    print_r($row);
     if($row && $row['expire']>time()){
+        //token验证通过
 
+        // 查询用户信息  并返回JSON
+        $sql = "select * from user where id = '{$row["id"]}'";
+//        echo $sql;
+        $res = $pdo->query($sql);
+        $data = $res->fetch(PDO::FETCH_ASSOC);
+//         print_r($data);
+        $response = [
+            'errno'=> 0,
+            'msg' => '验证通过',
+            'data' => $data
+        ];
+        echo json_encode($response,JSON_UNESCAPED_UNICODE);
     }else{
-
+        //  验证未通过
+        $response = [
+            'errno' => 40009,
+            'msg' => "验证未通过"
+        ];
+        echo json_encode($response,JSON_UNESCAPED_UNICODE);
     }
